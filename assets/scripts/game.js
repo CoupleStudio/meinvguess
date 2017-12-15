89dda49d0402a629a2d66821db8b34ee9a81698b
zhupengfei <zhupengfei@wm510.com>
2017-12-15 20:33:29 +0800

    加入猜拳算法与视频播放方法

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -8,6 +8,8 @@
 //  - [Chinese] http://www.cocos.com/docs/creator/scripting/life-cycle-callbacks.html
 //  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/life-cycle-callbacks/index.html
 
+var Type = require('utils');        //Type中包含win, lock, start
+
 cc.Class({
     extends: cc.Component,
 
@@ -35,7 +37,33 @@ cc.Class({
         guessAnim: {
             default: null,
             type: cc.Animation,
-        }
+        },
+
+        myGuess: {
+            default: null,
+            type: cc.Node,
+        },
+
+        spVS: {
+            default: null,
+            type: cc.Sprite,
+        },
+
+        spResult: {
+            default: null,
+            type: cc.Sprite,
+        },
+
+        spGuessMM: {
+            default: null,
+            type: cc.Sprite,
+        }, 
+
+        spGuessGG: {
+            default: null, 
+            type: cc.Sprite,
+        }, 
+
     },
 
     // LIFE-CYCLE CALLBACKS:
@@ -43,28 +71,25 @@ cc.Class({
     // onLoad () {},
 
     start () {
-        cc.log("haha");
+        var self = this;
+        this._final = -1;   //0代表输，1代表平局，2代表赢
+        this._status = true;    //true代表开始，false代表结束
+        this._level = 1;    //level从1开始，5为止
+        this.playVideo(Type.Start);
     },
 
     // update (dt) {},
 
-    videoPlayerEvent (sender, event) {
-        if(event === cc.VideoPlayer.EventType.READY_TO_PLAY) {
-            this.videoPlayer.play();
-            return;
-        } 
-        if(event === cc.VideoPlayer.EventType.COMPLETED) {
-            this.playGuessAnim();
-            return;
-        }
-        // else if(event === cc.VideoPlayer.EventType.CLICKED) {
-        //     this.videoPlayer.node.removeFromParent();
-        //     this.title.enabled = true;
+    onVideoPlayerEvent (sender, event) {
+        // if(event === cc.VideoPlayer.EventType.COMPLETED) {
+        //     this.playGuessAnim();
+        //     this.showMyGuess();
+        //     return;
         // }
+
     },
 
     playGuessAnim () {
-
         var animation = this.guessAnim;
         animation.node.active = !animation.node.active;
         cc.log('debug0');
@@ -79,7 +104,123 @@ cc.Class({
             animation.addClip(clip);
             animation.play('guess');
         });
+    },
+
+    showMyGuess () {
+        this.myGuess.active = !this.myGuess.active;
+    },
+
+    onbtnGuessClick (e) {
+        cc.log("debug2");
+        if(this._status != true) {
+            return;
+        }
+        var self = this;
+        var scissor = ["cloth", "scissor", "stone"];
+        var stone = ["scissor", "stone", "cloth"];
+        var cloth = ["stone", "cloth", "scissor"];
+
+        switch(e.target.name) {
+            case "btnScissor":
+            this.generateResult(scissor);
+            cc.loader.loadRes("icon_scissor_model_result", cc.SpriteFrame, (err, spriteFrame)=>{
+                self.spGuessGG.node.active = !self.spGuessGG.node.active;
+                self.spGuessGG.spriteFrame = spriteFrame;
+                self.spGuessGG.node.runAction(cc.moveTo(1, cc.p(150, 0)));
+            });
+            break;
+
+            case "btnStone":
+            this.generateResult(stone);
+            cc.loader.loadRes("icon_stone_model_result", cc.SpriteFrame, (err, spriteFrame)=>{
+                self.spGuessGG.node.active = !self.spGuessGG.node.active;
+                self.spGuessGG.spriteFrame = spriteFrame;
+                self.spGuessGG.node.runAction(cc.moveTo(1, cc.p(150, 0)));
+            });
+            break;
+
+            case "btnCloth":
+            this.generateResult(cloth);
+            cc.loader.loadRes("icon_cloth_model_result", cc.SpriteFrame, (err, spriteFrame)=>{
+                self.spGuessGG.node.active = !self.spGuessGG.node.active;
+                self.spGuessGG.spriteFrame = spriteFrame;
+                self.spGuessGG.node.runAction(cc.moveTo(1, cc.p(150, 0)));
+            });
+            break;
+
+            default:
+            return;
+        }
+    },
+        
+
+    generateGuess () {
+        var range = ['scissor', 'stone', 'cloth'];
+        var index = Math.floor(cc.random0To1()*3+0);
+        return range[index];
+    },
+
+    generateResult (arr) {
+        var self = this;
+        var guess = this.generateGuess();
+        var path = "icon_"+guess+"_user_result"
+        cc.loader.loadRes(path, cc.SpriteFrame, (err, spriteFrame)=>{
+            self.spGuessMM.node.active = !self.spGuessMM.node.active;
+            self.spGuessMM.spriteFrame = spriteFrame;
+            self.spGuessMM.node.runAction(cc.moveTo(1, cc.p(-150, 0)));
+        });
+
+        if(arr.indexOf(guess) == -1) {
+            return;
+        }
+        this._final = arr.indexOf(guess);
+        this.showResult(this._final);
+    },
+
+    showResult (final) {
+        var self = this;
+        var result = ["lost", "draw", "win"];
+        this.spVS.node.active = !this.spVS.node.active;
+        if(final == -1) {
+            return;
+        }
+        var path = "game_"+result[final]+"_icon";
+        cc.log(path);
+        cc.loader.loadRes(path, cc.SpriteFrame, (err, spriteFrame)=> {
+            self.spResult.spriteFrame = spriteFrame;
+            self.spResult.node.runAction(cc.moveTo(1, cc.p(0, -100)));
+        });
+
+        //停止guess动画
+        this.guessAnim.stop();
+        this.guessAnim.node.active = !this.guessAnim.node.active;
+
+        this._status = !this._status;
+    }, 
+
+    playVideo (index) {
+        var type = ["win", "start", "lock"];
+        if(type[index]!=undefined) {
+            var clip = "res/raw-assets/resources/video/"+this._level+ "_" + type[index]+".mp4";
+            cc.log('clipdebug');
+            cc.log(clip);
+            this.videoPlayer.clip = clip;
+            this.videoPlayer.play();
+            return;
+        }
+        return;
+    },
+
+
+
+    restart () {
+        this._status = !this._status;
+        this.guessAnim.node.active = !this.guessAnim.node.active;
+        this.guessAnim.play();
+
+        cc.log('restart');
     }
+
 
 
 });
---
+++ assets/scripts/utils.js
@@ -0,0 +1,7 @@
+var Type = cc.Enum({
+    Win: 0,
+    Start: 1,
+    lock: 2
+});
+
+module.exports = Type;
